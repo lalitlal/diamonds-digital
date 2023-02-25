610706d028c0b4126ea22c9647180e7150938973
Lalit Lal <lalitlal23@hotmail.com>
2023-02-25 13:54:20 -0500

    adding headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const headers = async () => {
+  return [
+    {
+      source: "/checkout",
+      headers: [
+        {
+          key: "Authorization",
+          value: `Bearer ${process.env.STRIPE_SECRET_KEY}`,
+        },
+      ],
+    },
+  ];
+};
+
+module.exports = { nextConfig, headers };
