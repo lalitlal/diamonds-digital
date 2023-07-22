00583db32cea8015fe1aca211f94519649d5d903
Lalit Lal <lalitlal23@hotmail.com>
2023-07-22 13:46:53 -0400

    updating env vars

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 import { apiVersion, dataset, projectId, useCdn, token } from "../env";
-
+// api token for writing docs
 export const client = createClient({
   apiVersion,
   dataset,
