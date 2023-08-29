ef165c04e769ac1fb15396d5138f745ec6d7c706
Lalit Lal <lalitlal23@hotmail.com>
2023-08-29 13:53:32 -0400

    bumping again

--- components/constants.jsx
+++ components/constants.jsx
@@ -1093,7 +1093,7 @@ export const minusIcon = (
   </svg>
 );
 
-export const marginMultiplier = 2.0 / 100.0; // convert to cents
+export const marginMultiplier = 2.2 / 100.0; // convert to cents
 
 export const toFixedDecimal = (num) => {
   return Number(num).toFixed(2);
