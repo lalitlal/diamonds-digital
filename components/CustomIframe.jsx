625aa539237162465f5e8aa55ef9d788c3d36c5a
Lalit Lal <lalitlal23@hotmail.com>
2023-08-31 18:59:33 -0400

    fixing custom iframe loader

--- components/CustomIframe.jsx
+++ components/CustomIframe.jsx
@@ -10,7 +10,7 @@ const CustomIframe = ({ url }) => {
     <iframe
       src={url}
       title="Embedded Content"
-      className={`w-full h-full ${iframeLoading ? "hidden" : ""}`}
+      className={`w-full h-fit lg:h-3/4 ${iframeLoading ? "hidden" : ""}`}
       onLoad={() => setIframeLoading(false)}
     ></iframe>
   );
@@ -34,25 +34,19 @@ const CustomIframe = ({ url }) => {
       )}
       {iframeContent}
       {modalVisible && iframeContent && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="fixed w-full h-full inset-0 flex items-center justify-center z-50">
           <div
             className="fixed inset-0 bg-black opacity-50"
             onClick={toggleModal}
           ></div>
-          <div className="modal bg-white p-4 rounded shadow-md w-1/2 h-1/2 relative overflow-hidden">
+          <div className="modal bg-white p-4 shadow-md w-4/5 h-fit lg:w-1/2 lg:h-3/4 relative overflow-hidden">
             <div
               className="absolute top-0 right-0 text-gray-600 cursor-pointer"
               onClick={toggleModal}
             >
               {xIcon}
             </div>
-            {iframeLoading ? (
-              <div className="w-full h-full bg-gray-200 animate-pulse">
-                Loading!
-              </div>
-            ) : (
-              iframeContent
-            )}
+            {iframeLoading ? <div></div> : iframeContent}
           </div>
         </div>
       )}
